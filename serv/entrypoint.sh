cc2f9b94157b6c3cfc95bbf6c94fb8974e11bfaa
pauclrt <p4ul.claret@gmail.com>
2024-11-03 09:13:02 -0600

    editing entrypoint script to run tests

--- serv/entrypoint.sh
+++ serv/entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 if [ "$1" = "test" ]; then
-    echo "Running tests..."
+    echo "Starting Gunicorn in background..."
     . /venv/bin/activate
+    gunicorn --bind 0.0.0.0:5000 app:app --workers 4 --threads 2 &
+    
+    # Wait briefly to ensure the server starts up
+    sleep 10
+    
+    echo "Running tests..."
     pytest
+    
+    # Bring the server back to the foreground
+    fg %1
 else
     echo "Starting Gunicorn..."
     exec gunicorn --bind 0.0.0.0:5000 app:app --workers 4 --threads 2
